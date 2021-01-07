2eeb0bf55d2b2c0306e063efafed99e3d1b41105
durn_ma <maximiliandurner@Maximilians-MBP.fritz.box>
2021-01-07 12:22:33 +0100

    playing around to read txt file

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,57 @@
     $("body").scrollspy({
         target: "#sideNav",
     });
+
+    // Initialize Popover (https://getbootstrap.com/docs/3.4/javascript/)
+    //$("[data-toggle=popover]").popover();
+    $("[data-toggle=popover]").popover({html:true});
+    //$('[data-toggle="popover"]').popover({
+    //html : true,
+    //content : function() {
+    //    return loadContent($(this).data('templatefile'))
+    //}
+
+    //$('#citeModal').on('show.bs.modal', function (event) {
+    //    var button = $(event.relatedTarget) // Button that triggered the modal
+    //    var file_contents = button.data('filecontents') // Extract info from data-* attributes
+    //    var modal = $(this)
+    //      modal.find('.modal-body').html(file_contents)
+    //})
+
+    $('#citeModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('filecontents') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').text(recipient)
+    });
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('filename') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        //modal.find('.modal-body').text(recipient)
+
+        var modal = $(this)
+          modal.find('.modal-body').html('cite.txt')
+
+        //jQuery.get('{% static 'cite.txt' %}',function readFile(data)
+        //{
+        //  modal.find('.modal-body').text(data)
+        //  //$('#outputText').val(data);
+        //  console.log(data);
+        //});
+
+    });
+
+
+
+
 })(jQuery); // End of use strict
+
+
